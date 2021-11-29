a41c694268635d42c3c0bd4fb55821cec76500f9
Sarmat <def2413@bk.ru>
2021-11-29 20:40:05 +0500

    BonusMile

--- src/main.java
+++ src/main.java
@@ -1,5 +1,8 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("123");
+        int price = 1112;
+        int bonusMile = price / 20;
+        System.out.println("Твой бонус: " + bonusMile);
+
     }
 }
